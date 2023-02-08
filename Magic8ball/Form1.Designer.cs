f41acbb52949f7db52af131ed2e38edc798f839c
Felix Edenborgh <31070311+FelixEdenborgh@users.noreply.github.com>
2023-02-08 15:46:37 +0100

    Add files via upload

---
+++ Magic8ball/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Magic8ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tellMeButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.awnserLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tellMeButton
+            // 
+            this.tellMeButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tellMeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tellMeButton.ForeColor = System.Drawing.Color.White;
+            this.tellMeButton.Location = new System.Drawing.Point(23, 1015);
+            this.tellMeButton.Name = "tellMeButton";
+            this.tellMeButton.Size = new System.Drawing.Size(283, 49);
+            this.tellMeButton.TabIndex = 0;
+            this.tellMeButton.Text = "Tell me!";
+            this.tellMeButton.UseVisualStyleBackColor = false;
+            this.tellMeButton.Click += new System.EventHandler(this.tellMeButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionBox.Location = new System.Drawing.Point(23, 965);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(283, 44);
+            this.questionBox.TabIndex = 1;
+            // 
+            // awnserLabel
+            // 
+            this.awnserLabel.BackColor = System.Drawing.Color.Transparent;
+            this.awnserLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.awnserLabel.ForeColor = System.Drawing.Color.White;
+            this.awnserLabel.Location = new System.Drawing.Point(829, 401);
+            this.awnserLabel.Name = "awnserLabel";
+            this.awnserLabel.Size = new System.Drawing.Size(261, 117);
+            this.awnserLabel.TabIndex = 2;
+            this.awnserLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1787, 1101);
+            this.Controls.Add(this.awnserLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.tellMeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tellMeButton;
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.Label awnserLabel;
+    }
+}
+
---
+++ Magic8ball/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8ball
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<String> answers = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+            // Nice awnsers
+            answers.Add("It is certain.");
+            answers.Add("It is decidedly so.");
+            answers.Add("Without a doubt.");
+            answers.Add("Yes definitely.");
+            answers.Add("You may rely on it.");
+            answers.Add("As I see it, yes.");
+            answers.Add("Most likely.");
+            answers.Add("Outlook good.");
+            answers.Add("Yes.");
+            answers.Add("Signs point to yes.");
+            // Semi ok awnsers
+            answers.Add("Reply hazy, try again.");
+            answers.Add("Ask again later.");
+            answers.Add("Better not tell you now.");
+            answers.Add("Cannot predict now.");
+            answers.Add("Concentrate and ask again.");
+            // Not nice awnsers
+            answers.Add("Don't count on it.");
+            answers.Add("My reply is no.");
+            answers.Add("My sources say no.");
+            answers.Add("Outlook not so good.");
+            answers.Add("Very doubtful.");
+        }
+
+        private void tellMeButton_Click(object sender, EventArgs e)
+        {
+            int index = random.Next(answers.Count);
+            awnserLabel.Text = answers[index];
+        }
+    }
+}
